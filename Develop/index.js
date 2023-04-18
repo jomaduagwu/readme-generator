ea9aa1652bf7589529d77125b8875954a9f34d9b
Ms Eko <jomaduagwu@gmail.com>
2023-04-18 18:30:56 -0500

    updates index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,54 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'project name',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Include any collaborators, third-party assets that require attribution, or links to tutorials followed.',
+    },
+    {
+        type: 'options',
+        name: 'license',
+        message: 'Select a license.',
+        choices: 
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'List any features your project has.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples of how to run tests for your application.',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile('README.md', data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
